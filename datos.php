358400d7f373643e144e81aac01702fcbc2a616a
PieroQuirel <114697431+PieroQuirel@users.noreply.github.com>
2023-04-15 12:32:16 -0500

    Add files via upload

---
+++ datos.php
@@ -0,0 +1,31 @@
+<?php
+
+$usuario = $_POST['usuario'];
+$password = $_POST['direccion'];
+$password = $_POST['email'];
+$password = $_POST['edad'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <?php
+            echo "<p>Hola! "$usuario"</p>";
+            echo "<p>Tu dirección es "$direccion"</p>";
+            echo "<p>Tienes "$edad" y tu correo es "$correo"</p>";
+        ?>
+        <a href="index.php" class="btn btn-danger">Regresar</a>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</body>
+</html>
